e33fab3a77133db170e29eb03e182b33d101ef18
ViVariy <80923239+ViVariy@users.noreply.github.com>
2021-04-01 13:08:54 +0300

    Create index.js

---
+++ quiz-lesson-3/index.js
@@ -0,0 +1,6 @@
+//Сколько миллисекунд пройдет с начала дня до 17:20 этого дня? 
+//Выберите верный вариант ответа. 
+//Созданный код разместите в GitHub в репозитории lessons, в ветке quiz-lesson-3 в папке quiz-lesson-3.
+let date_start = new Date(2021, 3, 1, 0, 0, 0);
+let date_end = new Date("2021-04-01 17:20:00");
+console.log(date_end - date_start)
